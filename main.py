deaef48d03929ce27eae2b939f01d75333fc00ad
George <morargeorge_02@yahoo.com>
2021-10-03 16:50:11 +0300

    tema lab 2

--- main.py
+++ main.py
@@ -1,16 +1,84 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def isPalindrome(nr):
+    """
+    Determina daca un numar este palindrom.
+    :param nr: nr intreg
+    :return: Returneaza True daca numarul este palindrom sau False in caz contrar.
+    """
+    nrNou = 0
+    cop = nr
+    if nr < 10:
+        return True
+    else:
+        while cop > 0:
+            nrNou = nrNou * 10 + cop % 10
+            cop = cop // 10
+    if nr == nrNou:
+        return True
+    return False
+
+assert isPalindrome(12) is False
+assert isPalindrome(121) is True
+assert isPalindrome(12344321) is True
+
+def isPrime(nr):
+    """
+    Determina daca un numar este prim
+    :param nr: nr intreg
+    :return: True daca numarul este prim sau False in caz contrar.
+    """
+    if nr < 2:
+        return False
+    else:
+        for i in range(2,nr // 2 + 1):
+            if nr % i == 0:
+                return False
+    return True
+
+assert isPrime(12) is False
+assert isPrime(2) is True
+assert isPrime(13) is True
+
+def isSuperPrime(nr):
+    """
+    Determina daca un numar este superprim.
+    :param nr: nr intreg
+    :return: True daca numarul este superprim sau False in caz contrar.
+    """
+    while nr > 0:
+        if not isPrime(nr):
+            return False
+        nr = nr // 10
+    return True
+
+assert isSuperPrime(233) is True
+assert isSuperPrime(237) is False
+assert isSuperPrime(239) is True
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main():
+
+    while True:
+
+        print("1.Determinat daca un numar este palindrom.")
+        print("2.Determinati daca un numar este super prim.")
+        print("3.Iesire")
+
+        optiune = input("Selectati optiune: ")
+
+        if optiune == "1":
+            nr = int(input("Introduceti numar: "))
+            print(isPalindrome(nr))
+        elif optiune == "2":
+            nr2 = int(input("Introduceti numar: "))
+            print(isSuperPrime(nr2))
+        elif optiune == "3":
+            break
+        else:
+            print("Optiune gresita! Va rugam selectati alta optiune.")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    main()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
